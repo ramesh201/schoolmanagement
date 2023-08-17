07ee292ec99e7eb93b7cbdfce6e43007864614d4
galaxcfreelancer <136368360+galaxcfreelancer@users.noreply.github.com>
2023-08-17 22:52:14 +0530

    about nav link removed

--- src/components/navbar/navigationbar.js
+++ src/components/navbar/navigationbar.js
@@ -39,7 +39,6 @@ class Navigationbar extends Component {
               <Offcanvas.Body>
                 <Nav className="justify-content-end flex-grow-1 pe-3">
                   <Nav.Link href="/">Home</Nav.Link>
-                  <Nav.Link href="/about">About</Nav.Link>
                   <Nav.Link onClick={() => this.logout()}>Logout</Nav.Link>
                 </Nav>
               </Offcanvas.Body>
